21a1e8afdc72fa2e1f6ab87f70a17b874338447c
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-08-30 15:27:42 +0100

    - how to feed cats page changed
    made to align with the rest of the site

--- resources.md
+++ resources.md
@@ -1,11 +1,13 @@
 
 #**Resources**
 
-*Tabby Cat by Loan*
+## Images
+
+*Tabby cat by Loan*
 
 https://unsplash.com/photos/7AIDE8PrvA0
 
-*Ginger Tabby on Windowsill by Florencia Viadana*
+*Ginger tabby on windowsill by Florencia Viadana*
 
 https://unsplash.com/photos/sT7ZNTQ8zHs
 
@@ -13,12 +15,32 @@ https://unsplash.com/photos/sT7ZNTQ8zHs
 
 https://unsplash.com/photos/1l2waV8glIQ
 
-*Sleeping Ginger by Iz & Phil*
+*Sleeping ginger cat by Iz & Phil*
 
 https://unsplash.com/photos/pdALzg0yN-8
 
-*Proud Ginger Cat by Lloyd Dirks*
+*Proud ginger cat by Lloyd Dirks*
 
 https://unsplash.com/photos/R1oSj2m-7Ks
 
+*Ginger cat*
+https://i.imgur.com/uEd6CDp.jpg
+
 Any other images on this site are mine.
+
+## Other Resources
+
+*Sitemap generator:*
+https://www.xml-sitemaps.com/
+
+*Responsive web design screenshot:*
+http://ami.responsivedesign.is/
+
+*Mockups (prototype):*
+https://www.canva.com
+
+*HTML validator*
+https://validator.w3.org/
+
+*Cat Emoji*
+https://cutekaomoji.com/animals/cats/
--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,33 @@
 /*
-The bare bones for the decreaseFont and increaseFont functions came from W3 Schools,
-but I adjusted the code to be more generic rather than the specific example provided
-on the page. I also got inspired to use it after doing Code Insitute's 5 Day Coding Challenge
-where the fontsyle property was used to zoom in and out of a font.
+The bare bones for the decreaseFont and increaseFont functions came from W3Schools,
+but I adjusted the code to be more generic rather than using the specific example provided by W3 schools. I also got inspired to use it after doing Code Institute's 5 Day Coding Challenge
+where the font style property was used to make a heading bigger or smaller.
 
 [4] W3 Schools, 'Javascript Font Style Property', 2020. [Online]. Available:
 https://www.w3schools.com/jsref/prop_style_fontsize.asp. [Accessed: 20-August-2020]
 
-The bare bones of the handle bar function come from the installation page of the official
+[2] Code Institute, '5 Day Coding Challenge', 2020. [Online]. Available:
+https://codeinstitute.net/5-day-coding-challenge/.[Accessed: 28-Aug-2020]
+
+The bare bones of the handle bar function came from the installation page of the official
 Handlebars page. I adjusted the code so that it would return the date in a specific format.
 
 [3] Handlebarsjs.com, 'Handlebars Installation', 2020. [Online]. Available:
 https://handlebarsjs.com/installation/. [Accessed: 20-Aug-2020].
 
-The code that directs to the index came from StackOverFlow. I obviously adjusted it so
-that it would return to the index page of this site. The same logic was used for the
-directToArticlesPage().
+The code that directs to the index page came from StackOverFlow. I obviously adjusted it so
+that it would return to the index page of my site. The same logic was used for the
+directToArticlesPage() function.
 
 [6] StackOverFlow, 'Return to Index', 2013. [Online]. Available:
 https://stackoverflow.com/questions/13158786/how-to-return-to-home-page-using-javascript/13158910. [Accessed: 20-August-2020]
 
-The bare bones of the validation came from W3 Schools, which I then modified
-to suit my validation needs, which was to ensure that a validation message would be passed
+The bare bones of the validation code came from W3Schools, which I then modified
+to suit my validation purposes, which was to ensure that a validation message/error would be passed
 depending on what the given user input was.
 
 [5] W3 Schools, 'Javascript Form Validation', 2020. [Online]. Available:
 https://www.w3schools.com/js/js_validation.asp. [Accessed: 20-August-2020]
-
-Beyond the general (admittedly, minor) adjustments, I avoided using variables such as x around
-and tried to name things as they were being used.
-
 */
 
 //increase font
@@ -37,7 +35,7 @@ function increaseFont() {
   document.getElementById("text").style.fontSize = "larger";
 }
 
-// decrease font
+//decrease font
 function decreaseFont() {
   document.getElementById("text").style.fontSize = "smaller";
 }
@@ -47,11 +45,12 @@ function directToIndex() {
   window.location.href = "index.html";
 }
 
+//return to the articles page
 function directToArticlesPage() {
   window.location.href = "articles.html";
 }
 
-// handle bars date function
+//handle bars date function
 $(function() {
 
   let html_template = $("#date-template").html();
@@ -68,7 +67,7 @@ $(function() {
   $('.content-placeholder').html(compiled_html);
 });
 
-// form validation handlebars
+//form validation
 function validateMessage() {
   let message_field = document.getElementById("msg").value;
   if (message_field == "" || message_field == null) {
